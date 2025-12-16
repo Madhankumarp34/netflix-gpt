39bb358dadeeb3eb02ede0cfefeaf5d9db8db499
Madhankumarp34 <madhankumarp34@gmail.com>
2025-12-16 13:29:52 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-amber-200 border-rad border-black m-5 shadow-2xl'>
+           <p className='font-bold text-3xl text-red-900 text-center'>Hellow World!</p>
+    </div>
+  )
+}
+
+export default App
